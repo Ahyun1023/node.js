17c8d7da079e01205ebff401ec9d919050625f81
ska6371 <ska6371@naver.com>
2019-10-18 22:34:32 +0900

    Update route.js

--- blog/routes/route.js
+++ blog/routes/route.js
@@ -1,14 +1,9 @@
 var moment = require('moment');
-
+var app = require('express');
 var alert = require('alert-node');
 
 var mysql_dbc = require('../DB/db')();
 var connection = mysql_dbc.init();
-
-var bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    static = require('serve-static'),
-    errorHandler = require('errorHandler');
 
 var signUp = function(req, res){
     console.log('회원가입 실행됨');
@@ -26,27 +21,62 @@ var signUp = function(req, res){
     var password_check = req.body.password_check || req.query.password_check
 
     if(users.password != password_check){
-        alert('잘못된 비밀번호');
+        alert('잘못된 비밀번호입니다.');
     }
     else{
-        connection.query('INSERT INTO users SET ?;', users, function(error, results, fields){
-            if(error){
-                console.log("error ocurred", error);
+        var select_sql = 'SELECT * FROM users WHERE id=?;';
+        connection.query(select_sql, [users.id], function(err, results){
+            console.log(results[0].id);
+            if(err){
+                console.log("error ocurred", err);
                 res.send({
                     "code": 400,
                     "failed": "error ocurred"
                 })
             }
+           
+            if(results[0].id.length > 0){
+                alert('동일한 아이디가 존재합니다.');
+            }
+            
             else{
-                for(var i = 0; i<results.length; i++){
-                    console.log('저장된 유저 정보: ' + results[i]);
-                }
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf-8'});
-                res.write('<h1>회원가입 성공</h1>');
-                res.write('<a href="../public/login.html">로그인 화면으로 이동</a>');
-                res.end();
+                var select_sql2 = 'SELECT * FROM users WHERE email=?;';
+                connection.query(select_sql2, [users.email], function(err, results){
+                    if(err){
+                        console.log("error ocurred", err);
+                        res.send({
+                            "code": 400,
+                            "failed": "error ocurred"
+                        })
+                    }
+
+                    if(users.email == results[0].email){
+                            alert('동일한 이메일이 존재합니다.');
+                        }
+
+                    else{
+                        var insert_sql = 'INSERT INTO users SET ?;';
+                        connection.query(insert_sql, users, function(err, results){
+                            if(err){
+                                console.log("error ocurred", err);
+                                res.send({
+                                    "code": 400,
+                                    "failed": "error ocurred"
+                                })
+                            }
+
+                            else{
+                                res.writeHead('200', {'Content-Type':'text/html;charset=utf-8'});
+                                res.write('<h1>회원가입 성공</h1>');
+                                res.write('<a href="../public/login.html">로그인 화면으로 이동</a>');
+                                res.end();
+                            }
+                        }) 
+                    }
+                })            
             }
         })
+        connection.end();
     }
 }
 
@@ -58,27 +88,53 @@ var login = function(req, res){
         "password": req.body.password || req.query.password
     }
 
-    var sql = "SELECT * FROM users;";
-    connection.query(sql, function(err, results, fields){
-        for(var i = 0; i<results.length; i++){
-            if(results.id[i] == users.id && results.password[i] == users.password){
+    var sql = "SELECT * FROM blog.users WHERE id=?;";
+    connection.query(sql, users.id, function(err, results, fields){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(results[0].id == users.id && results[0].password == users.password){
                     req.session.user = {
-                        id: id,
-                        name: results.name[i],
-                        authorized: true //근데 이건 뭐하는 거?
+                        id: results.id,
+                        name: results.name,
+                        authorized: true
                     }
                     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
                     res.write('<h1>로그인 성공</h1>');
-                    res.write('<p>id:'+id+'</p>');
-                    res.lwrite('<p>name:'+name+'</p>');
+                    res.write('<p>id:' + results[0].id + '</p>');
+                    res.write('<p>name:' + results[0].name + '</p>');
                     res.end();
                 }
-                else{
-                    alert("아이디 혹은 비밀번호가 틀렸습니다");
-            }
+
+            else if(req.session.user){
+                alert('이미 로그인되어 있습니다.');
+                }
+
+            else{
+                alert("아이디 혹은 비밀번호가 틀렸습니다");
+            }        
         }
     })
 }
 
+const logout = function(req, res){
+    console.log('로그아웃 실행됨');
+    if(req.session.user){
+        req.session.destroy(function (err){
+            if(err){
+                console.log(err);
+            }
+            alert('로그아웃 완료');
+            res.redirect('/public/login.html');
+        })
+    }
+    else{
+        alert('아직 로그인되어 있지 않습니다.');
+        res.redirect('/public/login.html');
+    }
+}
+
 module.exports.signUp = signUp;
-module.exports.login = login; //미완성
+module.exports.login = login;
+module.exports.logout = logout;
