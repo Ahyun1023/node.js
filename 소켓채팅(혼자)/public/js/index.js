a937d1f53012f15c1fdc281a3c26523474f32d57
남아현 <43693054+Ahyun1023@users.noreply.github.com>
2020-01-15 21:12:10 +0900

    Update index.js

--- 소켓채팅(혼자)/public/js/index.js
+++ 소켓채팅(혼자)/public/js/index.js
@@ -1,30 +1,27 @@
 var socket = io();
 
 /* 접속 되었을 때 실행 */
-socket.on('connect', function() {
+socket.on('connect', function () {
   var name = prompt('반갑습니다! 이름을 입력해주세요', '');
 
-  // /* 이름이 빈칸인 경우 */
-  // if(!name) {
- 
-  // }
+  /* 이름이 빈칸인 경우 */
+  socket.on('nullName', function () {
+    alert('이름을 입력해주세요');
+    location.reload(true);
+  })
+
+  socket.on('sameName', function () {
+    alert('같은 이름을 사용하고 있는 유저가 있습니다. 다른 이름을 입력해주세요.');
+    location.reload(true);
+  })
 
   /* 서버에 새로운 유저가 왔다고 알림 */
   socket.emit('newUser', name);
 })
 
-socket.on('nullName', function(){
-  alert('이름을 입력해주세요');
-  location.reload(true);
-})
-
-socket.on('error', function(){
-  alert('같은 이름을 사용하고 있는 유저가 있습니다. 다른 이름을 입력해주세요.');
-  location.reload(true);
-})
 
 /* 서버로부터 데이터 받은 경우 */
-socket.on('update', function(data) {
+socket.on('update', function (data) {
   var chat = document.getElementById('chat');
 
   var message = document.createElement('div');
@@ -32,18 +29,18 @@ socket.on('update', function(data) {
   var className = '';
 
   // 타입에 따라 적용할 클래스를 다르게 지정
-  switch(data.type) {
+  switch (data.type) {
     case 'message':
-      className = 'other'
-      break
+      className = 'other';
+      break;
 
     case 'connect':
-      className = 'connect'
-      break
+      className = 'connect';
+      break;
 
     case 'disconnect':
-      className = 'disconnect'
-      break
+      className = 'disconnect';
+      break;
   }
 
   message.classList.add(className);
@@ -53,21 +50,45 @@ socket.on('update', function(data) {
 
 /* 메시지 전송 함수 */
 function send() {
-  // 입력되어있는 데이터 가져오기
-  var message = document.getElementById('test').value;
-  
-  // 가져왔으니 데이터 빈칸으로 변경
-  document.getElementById('test').value = '';
+  let file = document.querySelector("#getfile");
+  file.onchange = function () {
+    let fileList = file.files;
 
-  // 내가 전송할 메시지 클라이언트에게 표시
+    let reader = new FileReader();
+    let img = reader.readAsDataURL(fileList[0]);
+
+    // 입력되어있는 데이터 가져오기
+    let message = document.getElementById('test').value;
+
+    // 가져왔으니 데이터 빈칸으로 변경
+    document.getElementById('test').value = '';
+
+
+    // 내가 전송할 메시지 클라이언트에게 표시
+    var chat = document.getElementById('chat');
+    var msg = document.createElement('div');
+    var node = document.createTextNode(message, img);
+    msg.classList.add('me');
+    msg.appendChild(node);
+    chat.appendChild(msg);
+
+    console.log(img);
+
+    // 서버로 message 이벤트 전달 + 데이터와 함께
+    socket.emit('message', { type: 'message', message: message, img: img });
+    //저 타입이랑 메세지가 data
+  }
+}
+
+function img_send() {
+  var url = document.getElementById('photo').value;
+
   var chat = document.getElementById('chat');
   var msg = document.createElement('div');
-  var node = document.createTextNode(message);
+  var node = document.createTextNode(url);
   msg.classList.add('me');
   msg.appendChild(node);
   chat.appendChild(msg);
 
-  // 서버로 message 이벤트 전달 + 데이터와 함께
-  socket.emit('message', {type: 'message', message: message});
-  //저 타입이랑 메세지가 data
+  socket.emit('message', { type: 'message', message: message });
 }
