fcd9d9caef2c8bf856a2f15d67e7fe0a04e70f62
남아현 <43693054+Ahyun1023@users.noreply.github.com>
2020-01-15 21:11:43 +0900

    Update app.js

--- 소켓채팅(혼자)/app.js
+++ 소켓채팅(혼자)/app.js
@@ -4,10 +4,14 @@ const static = require('serve-static');
 const socket = require('socket.io');
 const path = require('path');
 
+const multer = require('multer');
+const fs = require('fs');
+
 const mysql_dbc = require('./db/db')();
 const connection = mysql_dbc.init();
 
 const socket_route = require('./routes/chat');
+const img_route = require('./routes/img_route');
 
 const app = express();
 
@@ -21,64 +25,64 @@ app.use('/css', express.static('./public/css'));
 const server = http.createServer(app);
 const io = socket(server);
 
-/*const router = express.Router();
-app.use('/', router);*/
+const storage = multer.diskStorage({
+    destination : function(req, file, callback){
+        callback(null, 'uploads')
+    },
+    filename : function(req, file, callback){
+        callback(null, file.originalname);
+    }
+});
+
+const upload = multer({
+    storage : storage,
+    limits: {
+        files : 10,
+        fileSize : 1024 * 1024 * 1024
+    }
+});
 
 const onUser = [];
-let currentUser = 0;
 
 io.sockets.on('connection', function(socket){
     socket.on('newUser', function(name){
+        if(!name){
+            io.sockets.emit('nullName');
+        }
         socket.name = name;
         
         let sql = "SELECT name FROM users WHERE name=?";
-
         connection.query(sql, socket.name, function(err, results){
             if(err){
                 console.log(err);
             }
-            else{ //입력된 이름이 접속된 이름과 같을 때 
-                for(var i=0; i<onUser.length; i++){
-                    if(onUser[i] == socket.name){
-                        io.sockets.emit('error');
-                        break;
+
+            if(results.length != 0 && socket.name){//입력된 이름이 db에 있을 때
+                onUser.push(socket.name);
+                io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: socket.name+'님이 접속하셨습니다.'});
+            }
+
+            else if(results.length == 0 && socket.name){ //입력된 이름이 db에 없을 때
+                context = {
+                    id: socket.id,
+                    name: socket.name
+                }
+                sql = 'INSERT INTO users SET ?';
+                connection.query(sql, context, function(err, results){
+                    if(err){
+                        console.log(err);
                     }
-                }
-
-                if(!name){//이름이 입력되지 않았을 때
-                    io.sockets.emit('nullName');
-                }
-
-                else if(results.length != 0){//입력된 이름이 db에 있을 때
                     onUser.push(socket.name);
-                    currentUser++;
-                    console.log(currentUser);
-                    io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: socket.name+'님이 접속하셨습니다.'});
-                }
+                    io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: '새로운 유저, '+socket.name+'님이 접속하셨습니다.'});
 
-                else{ //입력된 이름이 db에 없을 때
-                    context = {
-                        id: socket.id,
-                        name: socket.name
-                    }
-                    sql = 'INSERT INTO users SET ?';
-                    connection.query(sql, context, function(err, results){
-                        if(err){
-                            console.log(err);
-                        }
-                        onUser.push(socket.name);
-                        currentUser++;
-                        console.log(currentUser);
-                        io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: '새로운 유저, '+socket.name+'님이 접속하셨습니다.'});
-
-                        connection.end();
-                    })
-                }
-            }  
+                    connection.end();
+                })
+            } 
         }) 
     })
 
     socket.on('message', function(data){
+        console.log(data);
         data.name = socket.name;
 
         if(data.message.indexOf('/s') != -1){
@@ -91,16 +95,14 @@ io.sockets.on('connection', function(socket){
                 if(err){
                     console.log(err);
                 }
-                // console.log(results[0].id);
-                // let info = io.sockets.connected[results[0].id];
-                // console.log(info);
 
                 if(results == 0){
                     io.to(socket.id).emit('update', {type: 'message', name:'SERVER', message: other_name + '이라는 이름이 존재하지 않습니다.'});
                 }
-                // else if(!io.sockets.connected[results[0].id]){
-                //     io.to(socket.id).emit('update', {type: 'message', name:'SERVER', message: other_name + '님은 접속상태가 아닙니다.'});
-                // }
+                else if(asdf){ //이미지가 포함되어 있을 때... app.post를 시키고 그렇게 받은 url을 broadcast를 시키고 app.get으로 이미지를 띄우라는데... 할 수 있을지...!
+
+                }
+
                 else{
                     io.to(results[0].id).emit('update', data);
                 }
@@ -110,13 +112,13 @@ io.sockets.on('connection', function(socket){
             socket.broadcast.emit('update', data);
         }
     })
-    
+
     socket.on('disconnect', function(){
-        onUser.splice(socket.name, 1);
-        currentUser--;
+        onUser.splice(onUser.indexOf(socket.name), 1);
+        console.log(onUser);
+
         io.sockets.emit('update', {type: 'disconnect', name:'SERVER', message: socket.name + '님이 퇴장하셨습니다.'});
     })
-    
 })
 
 server.listen(app.get('port'), function(){
